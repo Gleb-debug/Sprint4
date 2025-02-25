c0cbcff47edafa0430d12f16a138615a0c1487d1
Gleb-debug <soloviov.glebich@yandex.ru>
2025-02-25 22:30:54 +0500

    fix:исправление,добавлена параметризация

--- src/main/java/org/example/pages/MainPage.java
+++ src/main/java/org/example/pages/MainPage.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class MainPage {
     // Локаторы элементов
-    private final By questionAccordion = By.id("accordion__heading-0");
-    private final By answerAccordion = By.id("accordion__panel-0");
+    private final By questionAccordion = By.xpath("//*[contains(@id,'accordion__heading')]");
+    private final By answerAccordion = By.xpath("//*[contains(@id,'accordion__panel')]");
+    private final By acceptCookie = By.id("rcc-confirm-button");
     private final By orderButtonTop = By.className("Button_Button__ra12g");
     private final By orderButtonBottom = By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
     private final WebDriver driver;
@@ -23,17 +25,18 @@ public class MainPage {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public void clickQuestion() {
-        WebElement element = driver.findElement(questionAccordion);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        wait.until(ExpectedConditions.elementToBeClickable(questionAccordion)).click();
+    public void clickAcceptCookie() {
+        driver.findElement(acceptCookie).click();
+    }
+    public void clickQuestion(int index) {
+        List<WebElement> questions = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(questionAccordion));
+        questions.get(index).click();
     }
 
-    public String getAnswer() {
-        WebElement element = driver.findElement(answerAccordion);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        wait.until(ExpectedConditions.elementToBeClickable(answerAccordion)).click();
-        return element.getText();
+    public String getAnswer(int index) {
+        List<WebElement> answers = driver.findElements(answerAccordion);
+        wait.until(ExpectedConditions.visibilityOf(answers.get(index)));
+        return answers.get(index).getText();
     }
 
     public void clickOrderButtonTop() {
